7d006cad8167235bf86f346eeed0a560f45ba7b1
Austinkuria <kuriaaustin125@gmail.com>
2025-04-12 00:42:18 +0300

    Add search functionality to Navbar: implement search input and handle search submission; integrate search icon for mobile and desktop views.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Routes, Route } from 'react-router-dom';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeContext } from './context/ThemeContext';
 import { Box, CircularProgress } from '@mui/material';
+import SearchResults from './features/search/SearchResults';
 
 function App() {
   // Get the MUI theme from ThemeContext
@@ -17,6 +18,12 @@ function App() {
           <CircularProgress />
         </Box>
       }>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+          </Route>
+        </Routes>
         <Outlet />
       </Suspense>
     </MUIThemeProvider>
